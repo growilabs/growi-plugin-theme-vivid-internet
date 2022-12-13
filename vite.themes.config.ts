924a5f5d8d9d99c0af1a92554c782615304c13a4
Yuki Takei <yuki@weseek.co.jp>
2022-12-13 13:43:15 +0000

    reorganize build settings

--- vite.themes.config.ts
+++ vite.themes.config.ts
@@ -1,13 +1,14 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
+    outDir: 'dist/themes',
     manifest: true,
     rollupOptions: {
-      input: ['/client-entry.tsx'],
+      input: [
+        '/src/styles/style.scss',
+      ],
     },
   },
 });
