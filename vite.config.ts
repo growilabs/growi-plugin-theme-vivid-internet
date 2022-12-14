ed5d1c11fe9ed0811bc74dc5acdc7b3318426632
Yuki Takei <yuki@weseek.co.jp>
2022-12-14 10:55:01 +0000

    refactor project structure

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'dist/themes',
     manifest: true,
     rollupOptions: {
       input: [
